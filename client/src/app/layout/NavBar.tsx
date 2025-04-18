fa37a9985176198a069bfe83f669a21804c695c7
shevon2000 <shevonfernando0214@gmail.com>
2025-04-18 14:54:48 +0530

    Improve Update and Delete functionality using React Query - custom hooks & Axios interceptors

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -5,16 +5,16 @@ type Props = {
   openForm: () => void;
 }
 
-export default function NavBar({openForm}: Props) {
+export default function NavBar({ openForm }: Props) {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={{
         backgroundImage: 'linear-gradient(135deg, #1B5E20 0%, #388E3C 50%, #A5D6A7 100%)'
       }}>
         <Container maxWidth='xl'>
-          <Toolbar sx={{display: 'flex', justifyContent: 'space-between'}}>
+          <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
             <Box>
-              <MenuItem sx={{display: 'flex', gap: 2, color: '#E3DAC9'}}>
+              <MenuItem sx={{ display: 'flex', gap: 2, color: '#E3DAC9' }}>
                 <Group fontSize="large" />
                 <Typography variant="h4" fontWeight='bold'>
                   VibeLog
@@ -22,28 +22,28 @@ export default function NavBar({openForm}: Props) {
               </MenuItem>
             </Box>
 
-            <Box sx={{display: 'flex'}}>
+            <Box sx={{ display: 'flex' }}>
               <MenuItem sx={{
-                fontSize: '1.2rem', textTransform: 'uppercase', fontWeight: 'bold', color: '#E3DAC9' 
+                fontSize: '1.2rem', textTransform: 'uppercase', fontWeight: 'bold', color: '#E3DAC9'
               }}>
                 Content
               </MenuItem>
               <MenuItem sx={{
-                fontSize: '1.2rem', textTransform: 'uppercase', fontWeight: 'bold', color: '#E3DAC9' 
+                fontSize: '1.2rem', textTransform: 'uppercase', fontWeight: 'bold', color: '#E3DAC9'
               }}>
                 About
               </MenuItem>
               <MenuItem sx={{
-                fontSize: '1.2rem', textTransform: 'uppercase', fontWeight: 'bold', color: '#E3DAC9' 
+                fontSize: '1.2rem', textTransform: 'uppercase', fontWeight: 'bold', color: '#E3DAC9'
               }}>
                 Contact
               </MenuItem>
             </Box>
-            <Button 
-              size="large" 
-              variant="contained" 
-              color="warning" 
-              sx={{color: '#E3DAC9'}}
+            <Button
+              size="large"
+              variant="contained"
+              color="warning"
+              sx={{ color: '#E3DAC9' }}
               onClick={openForm}
             >
               Write Now
--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,48 +1,44 @@
-import { Grid2} from "@mui/material";
+import { Grid2 } from "@mui/material";
 import ActivityList from "./ActivityList";
 import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
 
 //receive props from parent component
 type Props = {
-    activities: Activity[]
-    selectActivity: (id: string) => void
-    cancelSelectActivity: () => void
-    selectedActivity?: Activity
-    openForm: (id: string) => void
-    closeForm: () => void
-    editMode: boolean
-    submitForm: (activity: Activity) => void
-    deleteActivity: (id: string) => void
+  activities: Activity[]
+  selectActivity: (id: string) => void
+  cancelSelectActivity: () => void
+  selectedActivity?: Activity
+  openForm: (id: string) => void
+  closeForm: () => void
+  editMode: boolean
 }
 
-export default function ActivityDashboard({activities, selectActivity, cancelSelectActivity, 
-  selectedActivity, openForm, closeForm, editMode, submitForm, deleteActivity }: Props) {
+export default function ActivityDashboard({ activities, selectActivity, cancelSelectActivity,
+  selectedActivity, openForm, closeForm, editMode }: Props) {
 
   return (
     <Grid2 container spacing={3}>
-        <Grid2 size={7}>
-            <ActivityList 
-              activities={activities} 
-              selectActivity={selectActivity}
-              deleteActivity={deleteActivity}
-            />
-        </Grid2>
-        <Grid2 size={5}>
-            {selectedActivity && !editMode && 
-              <ActivityDetails 
-                activity={selectedActivity} 
-                cancelSelectActivity={cancelSelectActivity}
-                openForm={openForm}
-              />
-            }
-            {editMode &&
-            <ActivityForm 
-              closeForm={closeForm} 
-              activity={selectedActivity}
-              submitForm={submitForm}
-            />}
-        </Grid2>
+      <Grid2 size={7}>
+        <ActivityList
+          activities={activities}
+          selectActivity={selectActivity}
+        />
+      </Grid2>
+      <Grid2 size={5}>
+        {selectedActivity && !editMode &&
+          <ActivityDetails
+            selectedActivity={selectedActivity}
+            cancelSelectActivity={cancelSelectActivity}
+            openForm={openForm}
+          />
+        }
+        {editMode &&
+          <ActivityForm
+            closeForm={closeForm}
+            activity={selectedActivity}
+          />}
+      </Grid2>
     </Grid2>
   )
 }
